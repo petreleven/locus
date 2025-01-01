0ea799721ffd265c21c18d6c458f3102af027c95
Peter Ndungu <93506710+petreleven@users.noreply.github.com>
2025-01-01 16:45:09 +0300

    Delete locusBase directory

--- locusBase/CMakeFiles/Core.dir/Core.cpp.o.d
+++
@@ -1,3 +0,0 @@
-locusBase/CMakeFiles/Core.dir/Core.cpp.o: \
- /home/peter/Desktop/tests/datadriven/locusBase/Core.cpp \
- /usr/include/stdc-predef.h
--- locusBase/CMakeFiles/LocusMathFunctions.dir/LocusMathFunctions.cpp.o.d
+++
@@ -1,3 +0,0 @@
-locusBase/CMakeFiles/LocusMathFunctions.dir/LocusMathFunctions.cpp.o: \
- /home/peter/Desktop/tests/datadriven/locusBase/LocusMathFunctions.cpp \
- /usr/include/stdc-predef.h
